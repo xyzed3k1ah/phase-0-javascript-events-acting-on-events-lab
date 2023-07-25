e494b615b1c2de96fc3b1d66f445cf0e927312e1
Zedikiah <xyzed3k1ah@gmail.com>
2023-07-25 11:37:29 -0600

    event acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#000000";
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
+dodger.style.left = "0px";
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // Assuming a container width of 400px
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
